5caaf2baa88282a804c292ac82c00194a31f8b1e
Jolas25 <oktant2012@gmail.com>
2023-06-16 23:50:21 +0300

    create modal

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { useState } from "react";
 import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div className="counter">
-        <h1 className="count">{count}</h1>
-        <div className="buttons">
-          <button
-            className="button minus"
-            onClick={() => {
-              setCount(count - 1);
-            }}
-          >
-            Minus
-          </button>
-          <button
-            className="button plus"
-            onClick={() => {
-              setCount(count + 1);
-            }}
-          >
-            Plus
-          </button>
-        </div>
+      <div className="modal">
+        <button>Відкрити</button>
       </div>
     </div>
   );
