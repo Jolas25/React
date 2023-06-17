df16931d38566cc3da24a8844dc12fb6b0183dc8
Jolas25 <oktant2012@gmail.com>
2023-06-17 15:54:54 +0300

    comlited quiz

--- src/App.js
+++ src/App.js
@@ -1,22 +1,90 @@
 import { useState } from "react";
 import "./App.scss";
-import Modal from "./Components/Modal";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ question, onClickVariant, step }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li key={text} onClick={() => onClickVariant(index)}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
-  const [open, setOpen] = useState(false);
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+
+  function onClickVariant(index) {
+    console.log(step, index);
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  }
+
   return (
     <div className="App">
-      <div className="modal__app">
-        <button
-          className="modal__button"
-          onClick={() => {
-            setOpen(true);
-          }}
-        >
-          Відкрити
-        </button>
-        {open && <Modal open={open} setOpen={setOpen} />}
-      </div>
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
