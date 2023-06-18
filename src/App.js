152cf9f9d54d31fdf1c9867103ba41363daaca04
Jolas25 <oktant2012@gmail.com>
2023-06-18 19:05:50 +0300

    comlited photoCollection

--- src/App.js
+++ src/App.js
@@ -1,32 +1,82 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
+import Collection from "./Collections";
+
+const cats = [
+  { name: "Все" },
+  { name: "Море" },
+  { name: "Горы" },
+  { name: "Архитектура" },
+  { name: "Города" },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [categoryId, setCategoryId] = useState(0);
+  const [searchValue, setSearchValue] = useState("");
+  const [collections, setCollections] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const category = categoryId ? `category=${categoryId}` : "";
+
+    fetch(
+      `https://648f1afe75a96b664444b456.mockapi.io/photo_collections?page=${page}&limit=3&${category}`
+    )
+      .then((res) => res.json())
+      .then((json) => setCollections(json))
+      .catch((err) => console.warn(err))
+      .finally(() => setIsLoading(false));
+  }, [categoryId, page]);
 
   return (
     <div className="App">
-      <div className="counter">
-        <h1 className="count">{count}</h1>
-        <div className="buttons">
-          <button
-            className="button minus"
-            onClick={() => {
-              setCount(count - 1);
-            }}
-          >
-            Minus
-          </button>
-          <button
-            className="button plus"
-            onClick={() => {
-              setCount(count + 1);
-            }}
-          >
-            Plus
-          </button>
-        </div>
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {cats.map((obj, i) => (
+            <li
+              key={obj.name}
+              className={categoryId === i ? "active" : ""}
+              onClick={() => setCategoryId(i)}
+            >
+              {obj.name}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
       </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Идет загрузка...</h2>
+        ) : (
+          collections
+            .filter((obj) => {
+              return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((obj, index) => (
+              <Collection key={index} name={obj.name} images={obj.photos} />
+            ))
+        )}
+      </div>
+      <ul className="pagination">
+        {[...Array(5)].map((_, i) => (
+          <li
+            key={i}
+            onClick={() => setPage(i + 1)}
+            className={page === i + 1 ? "active" : ""}
+          >
+            {i + 1}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
